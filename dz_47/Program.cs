798d11fc7b195751fb95f8414a46ac886aa3b411
Shmel <Shmel77713@yandex.ru>
2023-02-10 07:47:30 +0400

    update homework

---
+++ dz_47/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+//Заполнение двумерного массива
+double[,] FillMatrix(int rows, int cols)
+{
+    double[,] matr = new double[rows, cols];
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matr[i, j] = rand.NextDouble() * 10;
+        }
+    }
+    return matr;
+}
+
+void PrintMartix(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(Math.Round(matr[i, j], 1) + "\t");
+        }
+        System.Console.WriteLine();
+    }
+
+}
+double[,] matrix = FillMatrix(3, 4);
+System.Console.WriteLine();
+PrintMartix(matrix);
+
+
---
+++ dz_50/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает число, 
+// возвращает индексы этого элемента в двумерном массиве или же указание, что такого числа нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int[,] FillMatrix(int rows, int cols)
+{
+    int[,] matr = new int[rows, cols];
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matr[i, j] = rand.Next(0, 100);
+        }
+    }
+    return matr;
+}
+
+void PrintMartix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+
+}
+
+void CheckMatrixNumber(int[,] matr, int number)
+{
+    bool find_number = false;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (number == matr[i, j])
+            {
+                find_number = true;
+                break;
+            }     
+        }
+    }
+    if(find_number) System.Console.WriteLine("Такое число есть");
+    else System.Console.WriteLine("Такого числа нет");
+}
+
+int[,] matrix = FillMatrix(4, 3);
+System.Console.WriteLine();
+PrintMartix(matrix);
+System.Console.WriteLine("Введите Число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+CheckMatrixNumber(matrix, number);
+
